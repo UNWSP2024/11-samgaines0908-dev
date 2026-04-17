6e46059966e996081ca017e2837c55847f07d17f
samgaines0908-dev <sam.gaines0908@gmail.com>
2026-04-17 09:48:09 -0500

    update

---
+++ .github/GUI.py
@@ -0,0 +1,14 @@
+#Author: Sam Gaines
+#Date: 4/15/2026
+#Title: GUI Window
+
+import tkinter as tk
+
+window=tk.Tk()
+window.geometry("500x500")
+saying = "To give anything less than your best is to sacrifice the gift-Pre"
+
+label = tk.Label(window,text=saying, font=("Times New Roman", 25))
+label.pack()
+
+window.mainloop()
